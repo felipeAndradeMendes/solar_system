5f29abc1381d5cb1f7fe91c97f896c227305007c
Felipe Mendes <felipemadruga@gmail.com>
2023-01-24 14:42:20 -0300

    Conclui requisito 01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <>
+        <p>Sistema Solar</p>
+        <Header />
+      </>
+    );
   }
 }
 
 export default App;
-//
