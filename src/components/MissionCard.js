c5e8ad20eed8259b6061072f4c2adb50cc905f72
Felipe Mendes <felipemadruga@gmail.com>
2023-01-24 15:45:08 -0300

    Conclui requisito 09

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MissionCard extends React.Component {
   render() {
-    const  { name, year, country, destination } = this.props;
+    const { name, year, country, destination } = this.props;
     return (
       <div data-testid="mission-card">
         <p data-testid="mission-name">
@@ -21,5 +22,12 @@ class MissionCard extends React.Component {
     );
   }
 }
+
+MissionCard.propTypes = {
+  name: PropTypes.string,
+  year: PropTypes.number,
+  country: PropTypes.string,
+  destination: PropTypes.string,
+}.isRequired;
 
 export default MissionCard;
